0dc25aa46731111365f735e0e27dffc58d2104b0
AleksIvanov195 <alexsandar196@gmail.com>
2024-11-09 12:42:12 +0000

    style: Footer adjustments and navlinks

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import { NavLink } from 'react-router-dom';
 import './Navbar.scss';
 import Hamburger from 'hamburger-react';
 
@@ -14,8 +15,8 @@ export default function Navbar() {
 				<Hamburger toggled={isOpen} toggle={setIsOpen} />
 			</div>
 			<ul className={`navLinks ${isOpen ? 'show' : ''}`}>
-				<li><a>Home</a></li>
-				<li><a>Courses</a></li>
+				<li><NavLink to="/">Home</NavLink></li>
+				<li><NavLink to="/courses">Courses</NavLink></li>
 				<li><a>Topics</a></li>
 				<li><a>Exercises</a></li>
 			</ul>
