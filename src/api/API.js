65569038b585a989ac5a12c8fae6ea127b9063c5
AleksIvanov195 <alexsandar196@gmail.com>
2024-12-09 12:09:53 +0000

    feat: Include credentials, access & refresh token logic

--- src/api/API.js
+++ src/api/API.js
@@ -14,6 +14,7 @@ const callFetch = async (endpoint, method, data) => {
 		method: method,
 		headers: { 'Content-Type': 'application/json' },
 		...(data && { body: JSON.stringify(data) }),
+		credentials: 'include',
 	};
 
 	const endpointAddress = `${API_URL}${endpoint}`;
@@ -25,6 +26,9 @@ const callFetch = async (endpoint, method, data) => {
 		let result;
 		if (response.status === 204) {
 			result = null;
+		} else if (response.status === 401 && result && result.message === 'Access token expired') {
+			const refreshResponse = await API.post(`${API_URL}/users/refresh`);
+			if(refreshResponse.isSuccess) return callFetch(endpoint, method, data);
 		} else{
 			// In case data cannot be parsed as JSON
 			result = await response.json().catch(() => null);
