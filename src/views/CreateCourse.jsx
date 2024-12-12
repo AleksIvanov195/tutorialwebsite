d70d10abba1da668934b115af485557cfce6c438
AleksIvanov195 <alexsandar196@gmail.com>
2024-12-12 18:32:03 +0000

    fix: Issue with posting courses

--- src/views/CreateCourse.jsx
+++ src/views/CreateCourse.jsx
@@ -1,21 +1,29 @@
 import API from '../api/API';
 import CourseForm from '../components/enitity/forms/CourseForm';
 import CollapsiblePanel from '../components/UI/CollapsiblePanel';
+import { useState } from 'react';
+import { useAuth } from '../hooks/useAuth';
 
 export default function CreateCourse() {
 	// Inititalisation --------------------------------------------
+	const { authState } = useAuth();
 	// State ------------------------------------------------------
+	const [courseMessage, setCourseMessage] = useState('');
 	// Handlers ---------------------------------------------------
 	const handleCourseSubmit = async (data) => {
-		const response = await API.post('/courses', data);
-		console.log(response);
+		const response = await API.post('/courses', data, authState.isLoggedIn);
+		if (response.isSuccess) {
+			setCourseMessage('Course Creation successful!');
+		} else {
+			setCourseMessage(`Course Creation failed: ${response.message}`);
+		}
 	};
 
 	// View -------------------------------------------------------
 	return (
 		<>
 			<CollapsiblePanel header='Course Detail Form'>
-				<CourseForm onSubmit={handleCourseSubmit} />
+				<CourseForm onSubmit={handleCourseSubmit} courseMessage = {courseMessage}/>
 			</CollapsiblePanel>
 			<CollapsiblePanel header='Course Content Structure'>
 				<p>Here on one side we will have the available lessons and Quizzes and other content that can be added to the course.</p>
