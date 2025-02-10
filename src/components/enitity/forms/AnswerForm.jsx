07c8149f4e861d570413968e7e8f91ccf085b724
AleksIvanov195 <alexsandar196@gmail.com>
2025-02-10 13:46:29 +0000

    feat: ensure there is at least one correct answer selected

--- src/components/enitity/forms/AnswerForm.jsx
+++ src/components/enitity/forms/AnswerForm.jsx
@@ -1,6 +1,7 @@
 import Form from '../../UI/formui/Form';
 import useLoad from '../../../api/useLoad';
 import { useAuth } from '../../../hooks/useAuth';
+import toast from 'react-hot-toast';
 const AnswerForm = ({ header, onSubmit, onClose, question, mode = 'Edit' }) =>{
 	// Inititalisation --------------------------------------------
 	const { authState } = useAuth();
@@ -8,31 +9,37 @@ const AnswerForm = ({ header, onSubmit, onClose, question, mode = 'Edit' }) =>{
 	const [answers, setAnswers, answersMessage, isLoading, loadAnswers ] = useLoad(`/answers?AnswerQuestionID=${question.QuestionID}`, authState.isLoggedIn);
 	// Handlers ---------------------------------------------------
 	const handleFormSubmit = (data) => {
-		const currentAnswers = data.answers.map((answer, index) => {
-			if (isMultipleChoice) {
-				// For multiple choice questions, use the checked value to set the AnswerCorrect
-				const { checked, ...rest } = answer;
-				return {
-					...rest,
-					AnswerCorrect: checked,
-				};
-			} else {
-				// For single choice questions use the index of the correct asnwer provided by the radio button to set the AnswerCorrect
-				const { checked, ...rest } = answer;
-				const isChecked = parseInt(data.answers.checked) === index;
-				return {
-					...rest,
-					AnswerCorrect: isChecked,
-				};
-			}
-		});
-		const addedAnswers = currentAnswers.filter(answer => !answer.AnswerID);
-		const removedAnswers = answers.filter(originalAnswer => !currentAnswers.some(answer => answer.AnswerID === originalAnswer.AnswerID));
-		const updatedAnswers = currentAnswers.filter(answer => answer.AnswerID
-			&& answers.some(originalAnswer => originalAnswer.AnswerID === answer.AnswerID
-			&& originalAnswer.AnswerText !== answer.AnswerText
-			|| originalAnswer.AnswerCorrect !== answer.AnswerCorrect));
-		onSubmit({ addedAnswers, removedAnswers, updatedAnswers });
+		const hasCorrectAnswer = data.answers.some(answer => answer.checked);
+		if (!hasCorrectAnswer) {
+			toast.error('You must select at least 1 correct answer!');
+		}else{
+			const currentAnswers = data.answers.map((answer, index) => {
+				if (isMultipleChoice) {
+					// For multiple choice questions, use the checked value to set the AnswerCorrect
+					const { checked, ...rest } = answer;
+					return {
+						...rest,
+						AnswerCorrect: checked,
+					};
+				} else {
+					// For single choice questions use the index of the correct asnwer provided by the radio button to set the AnswerCorrect
+					const { checked, ...rest } = answer;
+					const isChecked = parseInt(data.answers.checked) === index;
+					return {
+						...rest,
+						AnswerCorrect: isChecked,
+					};
+				}
+			});
+			const addedAnswers = currentAnswers.filter(answer => !answer.AnswerID);
+			const removedAnswers = answers.filter(originalAnswer => !currentAnswers.some(answer => answer.AnswerID === originalAnswer.AnswerID));
+			const updatedAnswers = currentAnswers.filter(answer => answer.AnswerID
+				&& answers.some(originalAnswer => originalAnswer.AnswerID === answer.AnswerID
+				&& originalAnswer.AnswerText !== answer.AnswerText
+				|| originalAnswer.AnswerCorrect !== answer.AnswerCorrect));
+			onSubmit({ addedAnswers, removedAnswers, updatedAnswers });
+		}
+
 	};
 
 	const transformedAnswers = answers.map(answer => ({
@@ -60,6 +67,7 @@ const AnswerForm = ({ header, onSubmit, onClose, question, mode = 'Edit' }) =>{
 					header= {header}
 					dynamicFields={dynamicField}
 					isMultipleChoice={isMultipleChoice}
+					resetOnSubmit = {false}
 				/>
 			}
 		</>
