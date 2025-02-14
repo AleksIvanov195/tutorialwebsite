c7927a27493f85f0ef6c6cf94458860e17030f7a
AleksIvanov195 <alexsandar196@gmail.com>
2025-02-14 17:16:35 +0000

    feat: Content selector modal

---
+++ src/components/UI/modal/ContentSelectorModal.jsx
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+import SearchBar from '../SearchBar';
+import useLoad from '../../../api/useLoad';
+import Modal from './Modal';
+import { Button, ButtonTray } from '../Buttons';
+import './ContentSelectorModal.scss';
+const ContentSelectorModal = ({ endpoint, idField, textField, onClose, onSave }) => {
+	// State ------------------------------------------------------
+	const [searchString, setSearchString] = useState('');
+	const [items] = useLoad(`${endpoint}?search=${searchString}&searchFields=${textField}`);
+	const [selectedItems, setSelectedItems] = useState([]);
+	// Handlers ---------------------------------------------------
+	const addItem = (itemID) => setSelectedItems((prev) => [...prev, itemID]);
+	const removeItem = (itemID) => setSelectedItems((prev) => prev.filter((id) => id !== itemID));
+	const handleSelect = (itemID) => {
+		selectedItems.includes(itemID) ? removeItem(itemID) : addItem(itemID);
+	};
+
+	// View -------------------------------------------------------
+	return (
+		<Modal onClose={onClose}>
+			<div className="contentSelectorContainer">
+				<SearchBar searchString={searchString} setSearchString={setSearchString} placeholder="Search..." />
+				<ul>
+					{items.map((item) => (
+						<li key={item[idField]} onClick={() => handleSelect(item[idField])}>
+							<input
+								type="checkbox"
+								checked={selectedItems.includes(item[idField])}
+								onChange={() => handleSelect(item[idField])}
+								id={`checkbox-${item[idField]}`}
+							/>
+							<span>{item[textField]}</span>
+						</li>
+					))}
+				</ul>
+				<ButtonTray>
+					<Button onClick={() => onSave(selectedItems)}>Save</Button>
+					<Button onClick={onClose}>Close</Button>
+				</ButtonTray>
+			</div>
+		</Modal>
+	);
+};
+
+export default ContentSelectorModal;
