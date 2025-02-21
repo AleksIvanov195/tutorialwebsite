2727584125cc516d5b0c647ad3fab74c391db015
AleksIvanov195 <alexsandar196@gmail.com>
2025-02-21 13:59:39 +0000

    style: adjustment to buttons, form, modal

--- src/components/UI/modal/ContentSelectorModal.jsx
+++ src/components/UI/modal/ContentSelectorModal.jsx
@@ -4,10 +4,10 @@ import useLoad from '../../../api/useLoad';
 import Modal from './Modal';
 import { Button, ButtonTray } from '../Buttons';
 import './ContentSelectorModal.scss';
-const ContentSelectorModal = ({ endpoint, idField, textField, onClose, onSave }) => {
+const ContentSelectorModal = ({ endpoint, idField, textField, onClose, onSave, title }) => {
 	// State ------------------------------------------------------
 	const [searchString, setSearchString] = useState('');
-	const [items] = useLoad(`${endpoint}?search=${searchString}&searchFields=${textField}&orderby=${textField}`);
+	const [items,, itemsMessages, isItemsLoading] = useLoad(`${endpoint}?search=${searchString}&searchFields=${textField}&orderby=${textField}`);
 	const [selectedItems, setSelectedItems] = useState([]);
 	// Handlers ---------------------------------------------------
 	const addItem = (itemID) => setSelectedItems((prev) => [...prev, itemID]);
@@ -17,25 +17,42 @@ const ContentSelectorModal = ({ endpoint, idField, textField, onClose, onSave })
 	};
 
 	// View -------------------------------------------------------
+	const noContent = () => {
+		return itemsMessages;
+	};
+	const content = () => {
+		return (
+			<ul>
+				{items.map((item) => (
+					<li key={item[idField]} onClick={() => handleSelect(item[idField])}>
+						<input
+							type="checkbox"
+							checked={selectedItems.includes(item[idField])}
+							id={`checkbox-${item[idField]}`}
+						/>
+						<span>{item[textField]}</span>
+					</li>
+				))}
+			</ul>
+		);
+	};
 	return (
 		<Modal onClose={onClose}>
 			<div className="contentSelectorContainer">
+				<header className='contentSelectorHeader'>
+					<h1>{title}</h1>
+				</header>
 				<SearchBar searchString={searchString} setSearchString={setSearchString} placeholder="Search..." />
-				<ul>
-					{items.map((item) => (
-						<li key={item[idField]} onClick={() => handleSelect(item[idField])}>
-							<input
-								type="checkbox"
-								checked={selectedItems.includes(item[idField])}
-								id={`checkbox-${item[idField]}`}
-							/>
-							<span>{item[textField]}</span>
-						</li>
-					))}
-				</ul>
+				{
+					isItemsLoading || items.length == 0
+						?
+						noContent()
+						:
+						content()
+				}
 				<ButtonTray>
-					<Button onClick={() => onSave(selectedItems)}>Save</Button>
-					<Button onClick={onClose}>Close</Button>
+					<Button className={'formButton submitButton'} onClick={() => onSave(selectedItems)}>Save</Button>
+					<Button className={'formButton cancelButton'} onClick={onClose}>Close</Button>
 				</ButtonTray>
 			</div>
 		</Modal>
