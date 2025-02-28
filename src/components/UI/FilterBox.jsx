2715a212dd76d0c8ea47398a57a691c71254628a
AleksIvanov195 <alexsandar196@gmail.com>
2025-02-28 17:47:05 +0000

    refactor: FilterBox to use CollapsiblePanel component

--- src/components/UI/FilterBox.jsx
+++ src/components/UI/FilterBox.jsx
@@ -1,5 +1,5 @@
 import './FilterBox.scss';
-
+import CollapsiblePanel from './CollapsiblePanel';
 const FilterBox = ({ title, options, selectedValues, onChange }) => {
 	// Inititalisation --------------------------------------------
 	// State ------------------------------------------------------
@@ -7,20 +7,22 @@ const FilterBox = ({ title, options, selectedValues, onChange }) => {
 	// View -------------------------------------------------------
 	return (
 		<div className="filterBox">
-			<h4>{title}</h4>
-			<div className="filterOptions">
-				{options.map((option) => (
-					<label key={option.CoursecategoryID}>
-						<input
-							type="checkbox"
-							value={option.CoursecategoryName}
-							checked={selectedValues.includes(option.CoursecategoryName)}
-							onChange={() => onChange(option.CoursecategoryName)}
-						/>
-						{option.CoursecategoryName}
-					</label>
-				))}
-			</div>
+			<CollapsiblePanel title={title} titleSize='small'>
+				<div className="filterOptions">
+					{options.map((option) => (
+						<label key={option.CoursecategoryID}>
+							<input
+								type="checkbox"
+								value={option.CoursecategoryName}
+								checked={selectedValues.includes(option.CoursecategoryName)}
+								onChange={() => onChange(option.CoursecategoryName)}
+							/>
+							{option.CoursecategoryName}
+						</label>
+					))}
+				</div>
+			</CollapsiblePanel>
+
 		</div>
 	);
 };
