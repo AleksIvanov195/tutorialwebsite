9fabe71717ef4582943c7633a7b5a09974b00ed9
AleksIvanov195 <alexsandar196@gmail.com>
2025-03-10 17:07:57 +0000

    feat: Question preview update to be the same as quiz user view

--- src/components/enitity/quiz/QuizPreview.jsx
+++ src/components/enitity/quiz/QuizPreview.jsx
@@ -2,6 +2,7 @@ import { useState, useEffect } from 'react';
 import useLoad from '../../../api/useLoad';
 import Question from './Question';
 import { Button, ButtonTray } from '../../UI/Buttons';
+import QuestionList from './QuestionList';
 import './Quiz.scss';
 
 const QuizPreview = () => {
@@ -13,17 +14,27 @@ const QuizPreview = () => {
 	const [questionsData, setQuestionsData, questionsMessage, isLoading, loadQuestionsData] = useLoad(`/quizzes/${quizID}/questions-answers?orderby=QuestionOrdernumber,ASC`);
 	const [questionsAndAnswers, setQuestionsAndAnswers] = useState([]);
 	const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
+	// Stores the selected answers for the current question
 	const [selectedAnswers, setSelectedAnswers] = useState([]);
-	const [isSubmitted, setIsSubmitted] = useState(false);
-	const [isCorrect, setIsCorrect] = useState(null);
+	// Tracks whether the current question has been submitted
+	const [isQuestionSubmitted, setIsQuestionSubmitted] = useState(false);
+	// Tracks whether the submitted answer is correct.
+	const [isAnswerCorrect, setIsAnswerCorrect] = useState(null);
 	const [score, setScore] = useState(0);
 	const [quizFinished, setQuizFinished] = useState(false);
+	// Stores the correctness of each question.
+	const [correctAnswers, setCorrectAnswers] = useState([]);
 
 	useEffect(() => {
 		if (questionsData.length > 0) {
-			setQuestionsAndAnswers(transformQuestionsData());
+			const transformedData = transformQuestionsData();
+			setQuestionsAndAnswers(transformedData);
+			// Creates a new array with the same length with null values,
+			// These null values will be replaced by correctness of each question.
+			setCorrectAnswers(Array(transformedData.length).fill(null));
 		}
 	}, [questionsData]);
+
 	// Handlers ---------------------------------------------------
 	const transformQuestionsData = () => {
 		const transformedData = questionsData.map(item => ({
@@ -35,43 +46,78 @@ const QuizPreview = () => {
 		return sortedQuestionsAndAnswers;
 	};
 
+	// Handle selecting answers in the Question component
 	const handleSelectAnswer = (answerID) => {
-		setSelectedAnswers(prevSelected => {
-			if (prevSelected.includes(answerID)) {
-				return prevSelected.filter(ansID => ansID !== answerID);
+		setSelectedAnswers(prevSelectedAns => {
+			if (prevSelectedAns.includes(answerID)) {
+				return prevSelectedAns.filter(ansID => ansID !== answerID);
 			} else {
-				return [...prevSelected, answerID];
+				return [...prevSelectedAns, answerID];
 			}
 		});
 	};
 
 	const handleSubmit = () => {
+		// Cannot submit if there aren't any selected answers
 		if (selectedAnswers.length > 0) {
 			const currentQuestion = questionsAndAnswers[currentQuestionIndex];
 			const correctAnswerIDs = currentQuestion.Answers
 				.filter((ans) => ans.AnswerCorrect)
 				.map((ans) => ans.AnswerID);
 
-			const isAnswerCorrect = currentQuestion.QuestionType === 'MultipleChoice' ?
+			const answersCorrect = currentQuestion.QuestionType === 'MultipleChoice' ?
 				arraysMatch(selectedAnswers, correctAnswerIDs)
 				: correctAnswerIDs.includes(selectedAnswers[0]);
 
-			setIsSubmitted(true);
-			setIsCorrect(isAnswerCorrect);
+			setIsQuestionSubmitted(true);
+			setIsAnswerCorrect(answersCorrect);
 
-			if (isAnswerCorrect) {
+			if (answersCorrect) {
 				setScore((previous) => previous + 1);
 			}
+
+			// Update the correctAnswers state
+			setCorrectAnswers((prev) => {
+				const newCorrectAnswers = [...prev];
+				// Sets the correctnes of the answer
+				newCorrectAnswers[currentQuestionIndex] = answersCorrect;
+				return newCorrectAnswers;
+			});
 		}
 	};
 
+	// Handle going to next question
 	const handleNext = () => {
-		setIsSubmitted(false);
+		setIsQuestionSubmitted(false);
 		setSelectedAnswers([]);
-		if (currentQuestionIndex < questionsAndAnswers.length - 1) {
-			setCurrentQuestionIndex(previous => previous + 1);
+		goToNextQuestion();
+
+	};
+	const goToNextQuestion = () => {
+		let nextIndex = currentQuestionIndex + 1;
+		while (nextIndex < questionsAndAnswers.length && correctAnswers[nextIndex] !== null) {
+			nextIndex++;
+		}
+		if (nextIndex < questionsAndAnswers.length) {
+			setCurrentQuestionIndex(nextIndex);
 		} else {
-			setQuizFinished(true);
+			// If no unanswered questions are found, check from the beginning
+			nextIndex = correctAnswers.findIndex(answer => answer === null);
+			if (nextIndex !== -1) {
+				setCurrentQuestionIndex(nextIndex);
+			} else {
+				toast.success('You have answered all question, click Finish to finish the quiz!');
+			}
+		}
+	};
+	// Handle selecting a question from the question list
+	const handleQuestionClick = (index) => {
+		// if the question was not submitted
+		// This ensures that users cannot go back to a question if it was submitted.
+		if (correctAnswers[index] === null) {
+			setCurrentQuestionIndex(index);
+			setIsQuestionSubmitted(false);
+			setSelectedAnswers([]);
 		}
 	};
 
@@ -81,6 +127,8 @@ const QuizPreview = () => {
 		const sortedB = [...arr2].sort();
 		return sortedA.every((value, index) => value === sortedB[index]);
 	};
+
+	// ...existing code...
 
 	// View -------------------------------------------------------
 	if (isLoading || isQuizLoading) return <p>Loading...</p>;
@@ -97,26 +145,32 @@ const QuizPreview = () => {
 	}
 
 	const currentQuestion = questionsAndAnswers[currentQuestionIndex];
-
+	const allQuestionsAnswered = correctAnswers.every(answer => answer !== null);
 	return (
 		<div className="quizContainer">
+			<QuestionList
+				questions={questionsAndAnswers}
+				currentQuestionIndex={currentQuestionIndex}
+				onQuestionClick={handleQuestionClick}
+				correctAnswers={correctAnswers}
+			/>
 			<h2 className="quizHeader">Question {currentQuestionIndex + 1}:</h2>
 			<Question
 				question={currentQuestion}
 				selectedAnswers={selectedAnswers}
 				onSelectAnswer={handleSelectAnswer}
-				isSubmitted={isSubmitted}
+				isQuestionSubmitted={isQuestionSubmitted}
 			/>
-			{isSubmitted && (
-				<p className={`feedback ${isCorrect ? 'correct' : 'wrong'}`}>
-					{isCorrect ? 'Correct!' : `Wrong! ${currentQuestion.QuestionText}`}
+			{isQuestionSubmitted && (
+				<p className={`feedback ${isAnswerCorrect ? 'correct' : 'wrong'}`}>
+					{isAnswerCorrect ? 'Correct!' : `Wrong! ${currentQuestion.QuestionText}`}
 				</p>
 			)}
 			<ButtonTray>
-				<Button onClick={handleSubmit} disabled={isSubmitted || selectedAnswers === null}>Submit</Button>
-				{isSubmitted && (
-					<Button onClick={handleNext}>
-						{currentQuestionIndex < questionsAndAnswers.length - 1 ? 'Next' : 'Finish'}
+				<Button onClick={handleSubmit} disabled={isQuestionSubmitted || selectedAnswers === null}>Submit</Button>
+				{isQuestionSubmitted && (
+					<Button onClick={allQuestionsAnswered ? () => setQuizFinished(true) : handleNext}>
+						{allQuestionsAnswered ? 'Finish' : 'Next'}
 					</Button>
 				)}
 			</ButtonTray>
