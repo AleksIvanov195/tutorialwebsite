5baa6dda0299ab9501618f0a66d51d77c219c18b
AleksIvanov195 <alexsandar196@gmail.com>
2025-04-17 15:45:34 +0100

    feat: Course is automatically completed once all content in it is completed.

--- src/views/usercontentviews/Course.jsx
+++ src/views/usercontentviews/Course.jsx
@@ -1,28 +1,59 @@
 import useLoad from '../../api/useLoad';
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import { useLocation } from 'react-router-dom';
 import Lesson from './Lesson';
 import Quiz from './Quiz';
 import Animate from '../../components/UI/Animate';
 import { ContentPanel, ContentItem } from '../../components/UI/contentpanel/ContentPanel';
 import { useAuth } from '../../hooks/useAuth';
+import useApiActions from '../../hooks/useApiActions';
 import './Course.scss';
+
 const Course = () =>{
 	// Inititalisation --------------------------------------------
 	const { authState } = useAuth();
 	const location = useLocation();
+	const { put } = useApiActions();
 	const { courseID, userCourseID } = location.state || { courseID: null, userCourseID: null };
 	// State ------------------------------------------------------
 	const [course, setQuiz, quizMessage, isCourseLoading, loadQuiz] = useLoad(`/courses/${courseID}`);
+	const [userCourse,,, isUserCourseLoading, loadUserCourse] = useLoad(`/usercourses/${userCourseID}`);
 	const [courseContent, setCourseContent, , isLoading, loadCourseContent ] = useLoad(
 		authState.isLoggedIn
 			? `/coursecontents/simplified/user-completion?CoursecontentCourseID=${courseID}&orderby=CoursecontentOrder,ASC`
 			: `/coursecontents/simplified?CoursecontentCourseID=${courseID}&orderby=CoursecontentOrder,ASC`,
 	);
 	const [selectedCourseContent, setSelectedCourseContent] = useState(null);
+	const [isCourseCompleted, setIsCourseCompleted] = useState(false);
 	// Handlers ---------------------------------------------------
 	const handleItemClick = (content) => {
 		setSelectedCourseContent(content);
+	};
+	// Check if all course content is completed
+	useEffect(() => {
+		if (!isUserCourseLoading && userCourse.length > 0) {
+			setIsCourseCompleted(userCourse[0].UsercourseUsercontentstatusID === 3);
+		}
+		if (courseContent.length > 0 && userCourse.length > 0 && !isCourseCompleted) {
+			const allCompleted = courseContent.every(content => content.ContentStatus == true);
+			if(allCompleted && !isCourseCompleted) {
+				completeCourse();
+			}
+		}
+	}, [courseContent]);
+
+	const completeCourse = async () => {
+		if (isCourseCompleted) return;
+		const response = await put(`/usercourses/${userCourse[0].UsercourseID}/complete`, {
+			UsercourseUsercontentstatusID: 3,
+			UserlessonCompletiondate: new Date().toISOString().replace('T', ' ').split('.')[0],
+		}, {
+			successMessage: 'Course has been completed!',
+			errorMessage: 'Course could not be completed!',
+		});
+		if (response.isSuccess) {
+			loadUserCourse();
+		}
 	};
 	// View -------------------------------------------------------
 	// Content to view
@@ -34,7 +65,7 @@ const Course = () =>{
 		}
 		return null;
 	};
-	if(isLoading || isCourseLoading) {
+	if(isLoading || isCourseLoading || isUserCourseLoading) {
 		return(
 			<p>Loading content...</p>
 		);
