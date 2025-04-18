f72d58141202ed5386a22e35a8a53363ab08c3b0
AleksIvanov195 <alexsandar196@gmail.com>
2025-04-18 14:59:51 +0100

    feat: Pagination component

---
+++ src/components/UI/Pagination.jsx
@@ -0,0 +1,33 @@
+import { Button } from './Buttons';
+import Icons from './Icons';
+import './Pagination.scss';
+
+const Pagination = ({ currentPage, totalPages, onPageChange }) => {
+	// If no pages do not display the pagination
+	if (totalPages <= 1) return null;
+
+	return (
+		<div className={'pagination'}>
+
+			<Button
+				onClick={() => onPageChange(currentPage - 1)}
+				disabled={currentPage === 1}>
+				<Icons.Previous/>
+			</Button>
+
+			<div className="paginationInfo">
+					Page <span className="paginationCurrent">{currentPage}</span> of{' '}
+				<span className="paginationTotal">{totalPages}</span>
+			</div>
+
+			<Button
+				onClick={() => onPageChange(currentPage + 1)}
+				disabled={currentPage >= totalPages}>
+				<Icons.Next/>
+			</Button>
+
+		</div>
+	);
+};
+
+export default Pagination;
